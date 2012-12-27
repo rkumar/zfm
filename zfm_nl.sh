66499e110e8258b9abc830a3fef4869ef74c8452
Rahul Kumar <rahulbenegal@gmail.com>
2012-12-28 01:43:10 +0530

    tried out coloring of selection here, but moved code to zfm.zsh

--- zfm_nl.sh
+++ zfm_nl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 #  Date: 2012-12-17 
-#  Last update: 2012-12-21 20:14
+#  Last update: 2012-12-28 01:04
 #  A very quaint numering tool that numbers first 9 lines 
 #  and then uses first letter of file name or item as key
 # XXX we do use q for exiting, will have to think of something else
@@ -8,9 +8,13 @@ let c=1
 local patt='.'
 ##local defpatt='.'
 local defpatt=""
+local selct=$#selectedfiles
+echo "selected is $selct" 1>&2
 [[ $1 = "-p" ]] && { shift; patt="$1"; shift }
 # since string searching in zsh isn;t on regular expressions and ^ is not respected
 # i am taking width of match after removing ^ and using next char as next shortcut
+# # no longer required as i don't use grep, but i wish i still were since it allows better
+# matching
 patt=${patt:s/^//}
 local w=$#patt
 #let w++
@@ -35,6 +39,17 @@ while IFS= read -r line; do
             fi
         }
     fi
-    print -r -- "$sub) $line"
+    # only if there are selections we check against the array and color
+    # otherwise no check, remember that the cut that comes later can cut the 
+    # escape chars
+    if [[ $selct -gt 0 ]]; then
+        if [[ $selectedfiles[(i)$line] -gt $selct ]]; then
+            print -r -- "$sub) $line"
+        else
+            print -r -- "$sub) ${COLOR_BOLD}$line${COLOR_DEFAULT}"
+        fi
+    else
+        print -r -- "$sub) $line"
+    fi
     let c++
 done
