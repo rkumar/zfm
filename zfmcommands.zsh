1dc3e1c606e576ec748610b7709a09fd130e70f7
Rahul Kumar <rahulbenegal@gmail.com>
2012-12-29 14:51:39 +0530

    added delim so menu_loop can use delims

--- zfmcommands.zsh
+++ zfmcommands.zsh
@@ -5,13 +5,22 @@
 #       Author: rkumar http://github.com/rkumar/rbcurse/
 #         Date: 2012-12-26 - 15:13
 #      License: Freeware
-#  Last update: 2012-12-28 21:06
+#  Last update: 2012-12-29 00:03
 # ----------------------------------------------------------------------------- #
 
+# The delim you are using between commands. If commands use a space inside
+# then use comma or some such delim. Otherwise commands will be delimited on space.
+#
 ZFM_MY_DELIM=,
-export ZFM_MY_COMMANDS="ack,ag,tig stats,git stats"
+#
+# these are the commands that will be available when you press
+# MENU_KEY + c (i.e. backtick + c) and will operate without any file names provided
+# usually directory level. They are currently parsed using "read -A" and use IFS.
+#
+#
+ZFM_MY_COMMANDS="ack,ag,tig stats,git stats"
 # hotkeys for commands, put space if no hotkey
-export ZFM_MY_MNEM="agts"
+ZFM_MY_MNEM="agts"
 
 #  Now place functions for above commands, otherwise it is expected they
 #  are in path, if ZFM_xxx is first looked for, otherwise xxx in $PATH
@@ -33,6 +42,9 @@ ZFM_ag() {
     ag "$searchpattern"
     pause
 }
+#
+# remove the space when defining the function and add ZFM_ before it.
+#
 ZFM_tigstats() {
     # check for whether you have tig installed
     # If you have problems committing try setting GIT_EDITOR
@@ -41,6 +53,6 @@ ZFM_tigstats() {
     tig status
 }
 ZFM_gitstats() {
-    # check for whether you have tig installed
+    # check for whether you have git installed
     git status -sb | $PAGER
 }
