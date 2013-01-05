63d63aa8d0d67d7940050ffa17126b2b0763e693
Rahul Kumar <rahulbenegal@gmail.com>
2013-01-06 01:10:32 +0530

    q

--- zfmcommands.zsh
+++ zfmcommands.zsh
@@ -5,7 +5,7 @@
 #       Author: rkumar http://github.com/rkumar/rbcurse/
 #         Date: 2012-12-26 - 15:13
 #      License: Freeware
-#  Last update: 2013-01-03 20:40
+#  Last update: 2013-01-05 21:01
 # ----------------------------------------------------------------------------- #
 
 # The delim you are using between commands. If commands use a space inside
@@ -18,9 +18,9 @@ ZFM_MY_DELIM=,
 # usually directory level. They are currently parsed using "read -A" and use IFS.
 #
 #
-ZFM_MY_COMMANDS="ack,ag,tree,ffind,tig stats,git stats,locate,structure"
+ZFM_MY_COMMANDS="ack,ag,tree,ffind,tig stats,git stats,locate,structure,sitree"
 # hotkeys for commands, put space if no hotkey
-ZFM_MY_MNEM="a tfigls"
+ZFM_MY_MNEM="a tfiglse"
 
 #  Now place functions for above commands, otherwise it is expected they
 #  are in path, if ZFM_xxx is first looked for, otherwise xxx in $PATH
@@ -112,4 +112,15 @@ ZFM_locate() {
 }
 ZFM_mdfind() {
     perror "Not yet implemented, the results are usually too massive to be of use here"
+}
+ZFM_sitree() {
+    print "."
+    print -rl -- **/*(/N) | sed 's#/$##;s#/\([^/]*\)$#	\1#;s#\([^/]*/\)#    #g;s#\( *\)\(.*\)	#    \1|___#;s#^\([^ ]\)#|--  \1#;s#^ #| #'
+    ct=$( print -rl -- **/*(/N) )
+    if [[ -z "$ct" ]]; then
+        print "0 directories"
+    else
+        print "$(echo $ct | wc -l) directories"
+    fi
+    pause
 }
