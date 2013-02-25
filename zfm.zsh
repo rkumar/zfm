3313017582c6e18f5120b9cfd0896feb1bb6d52c
Rahul Kumar <rahulbenegal@gmail.com>
2013-02-25 20:35:15 +0530

    bump version

--- zfm.zsh
+++ zfm.zsh
@@ -7,7 +7,7 @@
 #       Author: rkumar http://github.com/rkumar/rbcurse/
 #         Date: 2012-12-17 - 19:21
 #      License: GPL
-#  Last update: 2013-02-25 14:49
+#  Last update: 2013-02-25 14:55
 #   This is the new kind of file browser that allows selection based on keys
 #   either chose 1-9 or drill down based on starting letters
 #
@@ -609,7 +609,7 @@ print -l -- "$str" | $PAGER
 function myzfm() {
     ##  global section
     ZFM_APP_NAME="zfm"
-    ZFM_VERSION="0.1.15-alnitak2"
+    ZFM_VERSION="0.1.15-alnitak4"
     M_TITLE="$ZFM_APP_NAME $ZFM_VERSION 2013/02/25"
     #  Array to place selected files
     typeset -U selectedfiles
